72f27ac2c327b8e8b966fa2ba769b5b6fc129b73
trucanh <22520082@gm.uit.edu.vn>
2025-05-13 10:52:09 +0700

    bo sung routes

---
+++ backend/routes/orderItem.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import orderItemController from '../controllers/orderItem.controller.js';
+import { protect, authorizeRoles } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', protect, authorizeRoles('admin'), orderItemController.getAllOrderItems);
+router.get('/:id', protect, authorizeRoles('admin'), orderItemController.getOrderItemById);
+
+export default router;
---
+++ backend/routes/payment.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import paymentController from '../controllers/payment.controller.js';
+import { protect } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.post('/', protect, paymentController.createPayment);
+router.get('/:orderId', protect, paymentController.getPaymentByOrderId);
+
+export default router;
