f384b1a46b5baddae55e72d61b7e91742bd7b2c6
trucanh <22520082@gm.uit.edu.vn>
2025-05-15 15:54:23 +0700

    deployment slot -> staging

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,29 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
+// Kiểm tra biến môi trường Mongo URI
+if (!process.env.MONGO_URI) {
+  console.error("MONGO_URI is not defined in the environment variables.");
+  process.exit(1); // Dừng tiến trình nếu không có MONGO_URI
+}
+
+// Kết nối cơ sở dữ liệu
+connectDB();
+
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/email", emailRoutes);
 
+// Serve frontend trong production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
